f07418d0e46646192505a3cf17e0b5feb98f1729
unknown <cschackerl@ISATVIEWW026.iscout.local>
2015-06-10 11:39:07 +0200

    add mongo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger =require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -23,6 +25,14 @@ app.use(stylus.middleware(
     }
 ));
 app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log('myfirstmean db opended');
+});
 
 app.get('/partials/:partialPath', function(req, res){
     res.render('partials/' + req.params.partialPath);
